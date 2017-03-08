8ab2eeedff1ce9166dd1281036aa94aa642e4720
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-08 12:02:17 +0900

    add 12.rb

---
+++ 12.rb
@@ -0,0 +1,7 @@
+# 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
+# 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+# 確認にはcutコマンドを用いよ
+
+str = File.read('hightemp.txt')
+File.open('col1.txt', "w").puts(str.lines.map{|line| line.chars.values_at(0) })
+File.open('col2.txt', "w").puts(str.lines.map{|line| line.chars.values_at(1) })
