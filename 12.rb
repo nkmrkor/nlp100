7168a52db6f8b28002cde2a78cd9737283296700
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-10 17:58:13 +0900

    bug fix

--- 12.rb
+++ 12.rb
@@ -1,6 +1,6 @@
 # 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
 # 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
-# 確認にはcutコマンドを用いよ
+# cut -c1 hightemp.txt
 
 str = File.read('hightemp.txt')
 File.open('col1.txt', "w").puts(str.lines.map{|line| line.chars.values_at(0) })
