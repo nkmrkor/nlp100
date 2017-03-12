c186db4ebc714fa4d14c97a5ee5d951d32c4f081
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-12 22:57:24 +0900

    add 18.rb

---
+++ 18.rb
@@ -0,0 +1,17 @@
+# 18. 各行を3コラム目の数値の降順にソート
+# 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
+
+str = File.read('hightemp.txt')
+lines = []
+str.each_line do |line|
+  lines.push(line.split(/\t|\n/))
+end
+
+lines.each{|line|
+  line[0],line[2] = line[2],line[0]
+}
+lines.sort!.each{|line|
+  line[0],line[2] = line[2],line[0]
+}
+
+p lines
