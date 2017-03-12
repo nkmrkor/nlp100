ccd1a764e8291affe6687a8a06547b784ca3e8ed
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-12 22:56:58 +0900

    add 16.rb

---
+++ 16.rb
@@ -0,0 +1,8 @@
+# 16. ファイルをN分割する
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のファイルを行単位でN分割せよ．同様の処理をsplitコマンドで実現せよ．
+str = File.read('hightemp.txt')
+lines = []
+str.each_line do |line|
+  lines.push(line)
+end
+lines.each_slice(lines.count / ARGV[0].to_i) {|s| p s }
