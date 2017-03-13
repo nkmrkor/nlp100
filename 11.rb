244ebf9427029037c029307c4962dfa8a128c974
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-13 17:34:21 +0900

    fix

--- 11.rb
+++ 11.rb
@@ -2,6 +2,5 @@
 # タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
 # sed -e "s/\t/ /g" hightemp.txt
 
-str1 = File.read('hightemp.txt')
-str2 = str1.gsub(/\t/, ' ')
-File.open('hightemp.txt', "w").puts(str2)
+puts File.read('hightemp.txt').gsub(/\t/, ' ')
+
