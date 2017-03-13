6dbd553eda0042b679c6ccfada1a136c9ab44e9f
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-13 10:57:03 +0900

    fix

--- 13.rb
+++ 13.rb
@@ -1,5 +1,5 @@
 # 13. col1.txtとcol2.txtをマージ
 # 12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
 
-puts open('col1.txt', "r").readlines[0].chars.join(open('col2.txt', "r").readlines[0])
 
+File.open("sample.txt", "w").puts File.read('col1.txt').gsub(/\n/,"\t").scan(/.{1,2}/).zip(File.read('col2.txt').split(/\n/)).join.scan(/.{1,3}/)
