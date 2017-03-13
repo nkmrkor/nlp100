9c17f181efebe7e2e180b2cb3267e6558ac923fa
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-13 11:16:07 +0900

    17.rb

--- 17.rb
+++ 17.rb
@@ -1,5 +1,4 @@
 # 17. １列目の文字列の異なり
 # 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
 
-str = File.read('hightemp.txt')
-p str.lines.map{|line| line.chars.values_at(0) }.uniq
+p File.read('hightemp.txt').lines.map{|line| line.chars.values_at(0) }.uniq
