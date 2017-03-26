71cf73d4baea151d35ebdbf6335c84341241642b
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-03-26 22:08:01 +0900

    bug fix for 24.rb

--- chapter_3/24.rb
+++ chapter_3/24.rb
@@ -3,21 +3,4 @@
 
 require 'json'
 
-file_path='./jawiki-country.json'
-parsed = []
-text = []
-
-open(file_path) do |file|
-    file.each_line do |line|
-        parsed.push(JSON.parse(line))
-    end
-end
-
-parsed.select{|item|
-  if item["title"] == "イギリス"
-    text = item["text"]
-  end
-}
-
-puts text.scan(/\[\[ファイル:.*/)
-
+puts File.open('jawiki-country.json').readlines.map{|x| JSON.parse(x)}.select{|item| item["title"]=="イギリス"}.to_s.scan(/(\[\[ファイル:.*?\]\])/)
